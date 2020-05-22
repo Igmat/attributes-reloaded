6dd6bccc43a618b5f3c9fbe0ad0bb89cb625d8ae
Ihor Chulinda <ichulinda@gmail.com>
2020-05-22 16:35:33 +0200

    fix: remove redundant bonuses on battle reward

--- Patches/Social/DefaultBattleRewardModelPatch.cs
+++ Patches/Social/DefaultBattleRewardModelPatch.cs
@@ -4,21 +4,21 @@ using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 
 namespace AttributesReloaded.Patches.Social
 {
-    [HarmonyPatch(typeof(DefaultBattleRewardModel))]
-    class DefaultBattleRewardModelPatch
-    {
-        [HarmonyPostfix]
-        [HarmonyPatch("CalculateInfluenceGain")]
-        public static float CalculateInfluenceGain(float __result, PartyBase party, float influenceValueOfBattle, float contributionShare, StatExplainer explanation = null) =>
-            __result * party.GetPartyBonus(
-                bonuses => bonuses.InfluenceMultiplier,
-                (bonus, name) => Logger.Log("Increase influence by " + bonus * __result + " for " + name + "'s SOC", party.Leader));
+    //[HarmonyPatch(typeof(DefaultBattleRewardModel))]
+    //class DefaultBattleRewardModelPatch
+    //{
+    //    [HarmonyPostfix]
+    //    [HarmonyPatch("CalculateInfluenceGain")]
+    //    public static float CalculateInfluenceGain(float __result, PartyBase party, float influenceValueOfBattle, float contributionShare, StatExplainer explanation = null) =>
+    //        __result * party.GetPartyBonus(
+    //            bonuses => bonuses.InfluenceMultiplier,
+    //            (bonus, name) => Logger.Log("Increase influence by " + bonus * __result + " for " + name + "'s SOC", party.Leader));
 
-        [HarmonyPostfix]
-        [HarmonyPatch("CalculateRenownGain")]
-        public static float CalculateRenownGain(float __result, PartyBase party, float renownValueOfBattle, float contributionShare, StatExplainer explanation = null) =>
-            __result * party.GetPartyBonus(
-                bonuses => bonuses.RenownMultiplier,
-                (bonus, name) => Logger.Log("Increase renown by " + bonus * __result + " for " + name + "'s SOC", party.Leader));
-    }
+    //    [HarmonyPostfix]
+    //    [HarmonyPatch("CalculateRenownGain")]
+    //    public static float CalculateRenownGain(float __result, PartyBase party, float renownValueOfBattle, float contributionShare, StatExplainer explanation = null) =>
+    //        __result * party.GetPartyBonus(
+    //            bonuses => bonuses.RenownMultiplier,
+    //            (bonus, name) => Logger.Log("Increase renown by " + bonus * __result + " for " + name + "'s SOC", party.Leader));
+    //}
 }
